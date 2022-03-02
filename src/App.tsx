64bd50e86d96da81116b5a7598cfd5566a7e26fc
Daniel Baker <danpbaker90@gmail.com>
2022-03-02 17:43:17 +0000

    Tidy up demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import queryString from 'query-string';
 import './App.css';
 
@@ -7,16 +7,16 @@ const App = () => {
   const DEFAULT_IFRAME_WIDTH = 750;
   const CHECKOUT_BASE_URL = 'https://ci-checkout.imburse.net';
 
-  enum Events {
-    COMPONENT_LOADED = 'imburse_checkout:component_loaded',
-    COMPONENT_RESIZE = 'imburse_checkout:component_resize',
-    SESSION_TOKEN_EXPIRED = 'imburse_checkout:session_token_expired',
-    SESSION_TOKEN_INVALID = 'imburse_checkout:session_token_invalid',
-    SESSION_ERROR = 'imburse_checkout:session_error',
-    PAYMENT_SUCCESS = 'imburse_checkout:payment_success',
-    PAYMENT_FAILURE = 'imburse_checkout:payment_failed',
-    UNSUPPORTED_LANGUAGE = 'imburse_checkout:unsupported_language'
-  };
+  const EVENTS = useMemo(() => Object.freeze({
+    COMPONENT_LOADED: 'imburse_checkout:component_loaded',
+    COMPONENT_RESIZE: 'imburse_checkout:component_resize',
+    SESSION_TOKEN_EXPIRED: 'imburse_checkout:session_token_expired',
+    SESSION_TOKEN_INVALID: 'imburse_checkout:session_token_invalid',
+    SESSION_ERROR: 'imburse_checkout:session_error',
+    PAYMENT_SUCCESS: 'imburse_checkout:payment_success',
+    PAYMENT_FAILURE: 'imburse_checkout:payment_failed',
+    UNSUPPORTED_LANGUAGE: 'imburse_checkout:unsupported_language'
+  }), []);
 
   const [iframeHeight, setIframeHeight] = useState(DEFAULT_IFRAME_HEIGHT);
   const [events, setEvents] = useState<any[]>([]);
@@ -24,30 +24,28 @@ const App = () => {
 
   useEffect(() => {
     const receiveMessage = ((event: any) => {
-
       console.log(event);
 
       if (
         event.origin !== CHECKOUT_BASE_URL ||
         !event.data ||
         !event.data.message ||
-        !Object.values(Events).includes(event.data.message)) {
+        !Object.values(EVENTS).includes(event.data.message)) {
         return;
       }
 
-      // console.log(event.data);
-
       setEvents((events => [...events, event.data]));
 
-      if (event.data.message === Events.COMPONENT_RESIZE) {
+      if (event.data.message === EVENTS.COMPONENT_RESIZE) {
         const { data: { payload: { height } } } = event;
 
         const newHeight = height > DEFAULT_IFRAME_HEIGHT ? height : DEFAULT_IFRAME_HEIGHT;
         setIframeHeight(newHeight);
       }
     });
+
     window.addEventListener('message', receiveMessage, false);
-  }, [Events]);
+  }, [EVENTS]);
 
   useEffect((): void => {
     const { token } = queryString.parse(window.location.search);
@@ -63,14 +61,14 @@ const App = () => {
 
   return (
     iframeSrc ?
-      <div className="App">
-        <div className="Item">
+      <div className="app">
+        <div className="item iframe">
           <h1>Iframe</h1>
           <iframe title="checkout" height={iframeHeight} width={DEFAULT_IFRAME_WIDTH} src={iframeSrc} />
         </div>
-        <div className="Item">
+        <div className="item events">
           <h1>Events</h1>
-          {events.map((event, index) => <div key={index} className="Event">{JSON.stringify(event, null, 2)}</div>)}
+          {events.map((event, index) => <div key={index} className="event">{JSON.stringify(event, null, 2)}</div>)}
         </div>
       </div> : null
   );
