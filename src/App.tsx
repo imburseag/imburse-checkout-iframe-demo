e1aa74e406042f85b6ca26966e00de52338fc989
Daniel Baker <danpbaker90@gmail.com>
2022-03-03 11:43:45 +0000

    Move token to constant

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState, useMemo } from 'react';
-import queryString from 'query-string';
+
 import './App.css';
 
 const App = () => {
   const DEFAULT_IFRAME_HEIGHT = 450;
   const DEFAULT_IFRAME_WIDTH = 750;
-  const CHECKOUT_BASE_URL = 'https://ci-checkout.imburse.net';
+  const CHECKOUT_BASE_URL = 'https://qa-checkout.imburse.net';
+  const TOKEN = 'YOUR_TOKEN';
+  const IFRAME_SRC = `${CHECKOUT_BASE_URL}?token=${TOKEN}`;
 
-  const EVENTS = useMemo(() => Object.freeze({
+  const eventTypes = useMemo(() => Object.freeze({
     COMPONENT_LOADED: 'imburse_checkout:component_loaded',
     COMPONENT_RESIZE: 'imburse_checkout:component_resize',
     SESSION_TOKEN_EXPIRED: 'imburse_checkout:session_token_expired',
@@ -20,7 +22,6 @@ const App = () => {
 
   const [iframeHeight, setIframeHeight] = useState(DEFAULT_IFRAME_HEIGHT);
   const [events, setEvents] = useState<any[]>([]);
-  const [iframeSrc, setIframeSrc] = useState<any>();
 
   useEffect(() => {
     const receiveMessage = ((event: any) => {
@@ -30,13 +31,13 @@ const App = () => {
         event.origin !== CHECKOUT_BASE_URL ||
         !event.data ||
         !event.data.message ||
-        !Object.values(EVENTS).includes(event.data.message)) {
+        !Object.values(eventTypes).includes(event.data.message)) {
         return;
       }
 
       setEvents((events => [...events, event.data]));
 
-      if (event.data.message === EVENTS.COMPONENT_RESIZE) {
+      if (event.data.message === eventTypes.COMPONENT_RESIZE) {
         const { data: { payload: { height } } } = event;
 
         const newHeight = height > DEFAULT_IFRAME_HEIGHT ? height : DEFAULT_IFRAME_HEIGHT;
@@ -45,32 +46,19 @@ const App = () => {
     });
 
     window.addEventListener('message', receiveMessage, false);
-  }, [EVENTS]);
-
-  useEffect((): void => {
-    const { token } = queryString.parse(window.location.search);
-
-    if (!token) {
-      return;
-    }
-
-    const src = `${CHECKOUT_BASE_URL}?token=${token}`;
-
-    setIframeSrc(src);
-  }, []);
+  }, [eventTypes]);
 
   return (
-    iframeSrc ?
-      <div className="app">
-        <div className="item iframe">
-          <h1>Iframe</h1>
-          <iframe title="checkout" height={iframeHeight} width={DEFAULT_IFRAME_WIDTH} src={iframeSrc} />
-        </div>
-        <div className="item events">
-          <h1>Events</h1>
-          {events.map((event, index) => <div key={index} className="event">{JSON.stringify(event, null, 2)}</div>)}
-        </div>
-      </div> : null
+    <div className="app">
+      <div className="item iframe">
+        <h1>Iframe</h1>
+        <iframe title="checkout" height={iframeHeight} width={DEFAULT_IFRAME_WIDTH} src={IFRAME_SRC} />
+      </div>
+      <div className="item events">
+        <h1>Events</h1>
+        {events.map((event, index) => <div key={index} className="event">{JSON.stringify(event, null, 2)}</div>)}
+      </div>
+    </div>
   );
 }
 
